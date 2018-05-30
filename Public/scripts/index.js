d5cef705812f884862f7fd6af0888c5b8b7604ff
Jared Sorge <jared@jsorge.net>
2018-05-29 20:30:02 -0700

    Making fonts local

--- Public/scripts/index.js
+++ Public/scripts/index.js
@@ -13,12 +13,3 @@
     });
 
 }(jQuery));
-
-/* TypeKit */
-(function(d) {
-	var config = {
-	  kitId: 'xko3hrq',
-	  scriptTimeout: 3000
-	},
-	h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-})(document);
