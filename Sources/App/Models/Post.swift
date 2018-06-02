0a435f122581da6787923f1bac5793914cb77f48
Jared Sorge <jared@jsorge.net>
2018-06-02 07:19:24 -0700

    Displaying dates properly

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -13,33 +13,38 @@ struct BasePost {
 }
 
 struct Post: Codable {
-    var date: Date {
-        return frontMatter.date
-    }
-    var formattedDate: String? {
+    static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        return formatter.string(from: date)
-    }
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
+    let date: Date
+    let formattedDate: String
     let url: String
     let title: String?
     let content: String
+    let isBlogPost: Bool
     let frontMatter: FrontMatter
     
     init(url: String, title: String?, content: String, frontMatter: FrontMatter) {
+        self.date = frontMatter.date
+        self.formattedDate = Post.dateFormatter.string(from: frontMatter.date)
         self.url = url
         self.title = title
         self.content = content
+        self.isBlogPost = (frontMatter.isStaticPage == false)
         self.frontMatter = frontMatter
     }
 }
 
 struct FrontMatter: Codable {
-    let isMicroblog: Bool = false
+    let isMicroblog: Bool
     let title: String?
     let layout: String?
     let guid: String?
     let date: Date
-    let isStaticPage: Bool = false
+    let isStaticPage: Bool
     
     private enum CodingKeys: String, CodingKey {
         case isMicroblog = "microblog"
@@ -47,5 +52,16 @@ struct FrontMatter: Codable {
         case layout = "layout"
         case guid = "guid"
         case date = "date"
+        case isStaticPage = "staticpage"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.isMicroblog = try container.decodeIfPresent(Bool.self, forKey: .isMicroblog) ?? false
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+        self.layout = try container.decodeIfPresent(String.self, forKey: .layout)
+        self.guid = try container.decodeIfPresent(String.self, forKey: .guid)
+        self.date = try container.decode(Date.self, forKey: .date)
+        self.isStaticPage = try container.decodeIfPresent(Bool.self, forKey: .isStaticPage) ?? false
     }
 }
