d1732daca5918eda7c188e72fe7586b40b479dd0
Jared Sorge <jared@jsorge.net>
2018-06-05 12:41:47 -0700

    Regenerating feeds using the event loop

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -5,5 +5,18 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
 public func boot(_ app: Application) throws {
-    try FeedOutput.makeAllTheFeeds()
+    runRepeatedTask(app)
+}
+
+private func runRepeatedTask(_ app: Application) {
+    _ = app.eventLoop.scheduleTask(in: .seconds(10)) { () -> Void in
+        do {
+            try FeedOutput.makeAllTheFeeds()
+        }
+        catch {
+            print("Unable to make all the feeds")
+        }
+        
+        runRepeatedTask(app)
+    }
 }
