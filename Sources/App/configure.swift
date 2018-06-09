4a23a536cc99294285199001b0b4e0cc486e5207
Jared Sorge <jared@jsorge.net>
2018-06-08 22:25:57 -0700

    Moved view templates to the public folder

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,7 @@
+import Core
 import Leaf
+import Service
+import TemplateKit
 import Vapor
 
 /// Called before your application initializes.
@@ -15,9 +18,46 @@ public func configure(
     services.register(router, as: Router.self)
 
     // Configure the rest of your application here
-    try services.register(LeafProvider())
+    try services.register(MaverickLeafProvider())
 
     var middleware = MiddlewareConfig.default()
     middleware.use(FileMiddleware.self)
     services.register(middleware)
 }
+
+public final class MaverickLeafProvider: Provider {
+    /// See Service.Provider.repositoryName
+    public static let repositoryName = "leaf"
+    
+    public init() {}
+    
+    /// See Service.Provider.Register
+    public func register(_ services: inout Services) throws {
+        services.register([TemplateRenderer.self, ViewRenderer.self]) { container -> LeafRenderer in
+            let config = try container.make(LeafConfig.self)
+            return LeafRenderer(
+                config: config,
+                using: container
+            )
+        }
+        
+        services.register { container -> LeafConfig in
+            let dir = try container.make(DirectoryConfig.self)
+            return try LeafConfig(
+                tags: container.make(),
+                viewsDir: dir.workDir + "Public/Views",
+                shouldCache: container.environment != .development
+            )
+        }
+        
+        services.register { container -> LeafTagConfig in
+            return LeafTagConfig.default()
+        }
+    }
+    
+    /// See Service.Provider.boot
+    public func didBoot(_ container: Container) throws -> Future<Void> {
+        return .done(on: container)
+    }
+}
+
