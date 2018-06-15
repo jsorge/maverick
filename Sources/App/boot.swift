f8348c0b2c5de4b2e05dded3a119e11d0536f19b
Jared Sorge <jared@jsorge.net>
2018-06-15 15:10:48 -0700

    warming the incoming paths on boot

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -5,6 +5,7 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
 public func boot(_ app: Application) throws {
+    try PathHelper.prepTheTemporaryPaths()
     runRepeatedTask(app)
 }
 
